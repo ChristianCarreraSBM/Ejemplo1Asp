dc4befa9938c2f59484970c129bb06796cf344d2
ChristianCarreraSBM <cridani15@gmail.com>
2022-10-26 19:48:51 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/OperasBasController.cs
@@ -0,0 +1,70 @@
+﻿using Ejemplo1Asp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo1Asp.Controllers
+{
+    public class OperasBasController : Controller
+    {
+        // GET: OperasBas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MuestraPeliculas()
+        {
+            var PeliculasService = new PeliculasService();
+            var model = PeliculasService.ObtenerPelicula();
+            return View();
+        }
+        public ActionResult Sumar()
+        {
+            var tem = new Calculos();
+            return View(tem);
+        }
+
+
+        [HttpPost]
+        public ActionResult Sumar(Calculos calculos, string r1)
+        {
+            var tem = new Calculos();
+            tem = calculos;
+            if (r1.Equals("Sumar"))
+            {
+                tem.Suma();
+            }
+            if (r1.Equals("Restar"))
+            {
+                tem.Resta();
+            }
+            ViewBag.res = tem.Resultado;
+            return View();
+        }
+
+         public RedirectToRouteResult Temporal()
+        {
+
+            /*var alumno1 = new Alumno() { Nombre = "Iza", edad = 15 };
+            var alumno2 = new Alumno() { Nombre = "Iza", edad = 15 };
+            return Json(new List<Alumno>() { alumno1, alumno2},
+                                                        JsonRequestBehavior.AllowGet);*/
+            //return View();
+
+            /*return Redirect("https://google.com.mx");*/
+
+            return RedirectToAction("About","Home");
+
+        }
+
+        public ActionResult vista()
+        {
+            ViewBag.Nombre = "Sofia";
+            ViewData["Nombre"] = "Iza";
+            return View();
+        }
+    }
+}
---
+++ Services/PeliculasService.cs
@@ -0,0 +1,37 @@
+﻿using Ejemplo1Asp.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo1Asp.Services
+{
+    public class PeliculasService
+    {
+        public List<Peliculas> ObtenerPelicula()
+        {
+            var pelicula1 = new Peliculas()
+            {
+                Titulo = "Super man",
+                Duración = 120,
+                Pais ="USA",
+                Publicacion = new DateTime (2013, 02, 5)
+
+            };
+            var pelicula2 = new Peliculas()
+            {
+                Titulo = "Spider Man",
+                Duración = 150,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 05, 5)
+
+            };
+            return new List<Peliculas> { pelicula1, pelicula2 };
+        }
+        
+
+
+    }
+
+}
